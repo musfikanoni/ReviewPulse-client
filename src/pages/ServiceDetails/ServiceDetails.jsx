9640e1bdff61f083ba9669cd7133ca3d1d968844
musfikanoni <musfikanoni@gmail.com>
2024-12-27 01:30:51 +0600

    Created service details

--- src/pages/ServiceDetails/ServiceDetails.jsx
+++ src/pages/ServiceDetails/ServiceDetails.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import DatePicker from 'react-datepicker';
 import { Helmet } from 'react-helmet';
 import { useLoaderData } from 'react-router-dom';
+import AuthContext from '../../context/AuthContext/AuthContext';
 
 const ServiceDetails = () => {
+    const {user} = useContext(AuthContext);
+    const [startDate, setStartDate] = useState(new Date());
     const service = useLoaderData();
     const {photoUrl, title, description, category, price, companyName, website} = service;
     return (
@@ -12,39 +16,52 @@ const ServiceDetails = () => {
                 <title>Service Details - ReviewPulse Service Website</title>
                 <link rel="canonical" href="http://mysite.com/example" />
             </Helmet>
-            <div className="flex">
-            <div className="hero">
+            <div className="lg:flex lg:gap-5 lg:mx-0 mx-5">
                 <div className="hero-content rounded-xl border flex-col lg:flex-row">
-                    <img className='rounded-xl'
+                    <img className='rounded-xl max-w-xs h-[35vh]'
                     src={photoUrl} />
                     <div>
                     <h2 className="text-xl font-bold">{title}</h2>
-                    <p className="">{description}</p>
-                    <p className="">{category}</p>
-                    <p className="">{companyName}</p>
-                    <p className="">{website}</p>
-                    <p className="">{price.price} {price.currency}</p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <p className="font-medium"><span className='font-bold'>Description:</span> {description}</p>
+                    <p className="font-medium"><span className='font-bold'>Category:</span> {category}</p>
+                    <p className="font-medium"><span className='font-bold'>Company Name:</span> {companyName}</p>
+                    <p className="font-medium"><span className='font-bold'>Website:</span> {website}</p>
+                    <p className="font-medium"><span className='font-bold'>Price:</span> {price.price} {price.currency}</p>
+                    {/* <button className="btn btn-primary">Get Started</button> */}
                     </div>
                 </div>
-            </div>
-            {/* <div className="hero bg-base-200 min-h-screen">
-                <div className="hero-content flex-col lg:flex-row">
-                    <img
-                    src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-                    className="max-w-sm rounded-lg shadow-2xl" />
-                    <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
-                    <button className="btn btn-primary">Get Started</button>
-                    </div>
+                <div className="rounded-xl border mt-7 lg:mt-0 lg:w-9/12 w-full max-w-3xl">
+                    <form className="card-body">
+                        <h3 className='font-bold text-lg'>Add Your Review</h3>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Post Review</span>
+                            </label>
+                            <textarea className="textarea textarea-bordered" placeholder="Review write here"></textarea>
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Review Posted Date</span>
+                            </label>
+                            <DatePicker readOnly selected={startDate} onChange={(date) => setStartDate(date)}  dateFormat="dd/MM/yyyy"
+                            placeholderText="Select a date" placeholder="Date" name='date' 
+                            className='lg:w-[346px] md:w-[345px] w-[325px] max-w-sm outline-gray-300 border border-gray-300 py-[11px] px-2.5 rounded-md'/>
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">User Email</span>
+                            </label>
+                            {/* <input type="email" readOnly name='email' defaultValue={user.email} placeholder="Price"
+                            className="input input-bordered" /> */}
+                        </div>
+                        <div className="form-control mt-6">
+                        <button className="btn btn-primary">Add Review</button>
+                        </div>
+                    </form>
                 </div>
-            </div> */}
             </div>
         </div>
+
     );
 };
 
